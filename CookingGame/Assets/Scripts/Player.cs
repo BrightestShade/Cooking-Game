27a9e8ca75a62233437c42e94657d1b7db6f0120
BrightestShade <raphaelwood9@gmail.com>
2026-07-02 13:12:15 +0100

    Cinemachine + Refactored player input for new input system + collision Detection

--- CookingGame/Assets/Scripts/Player.cs
+++ CookingGame/Assets/Scripts/Player.cs
@@ -2,41 +2,64 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
+   
 
     private bool isWalking;
 
     private void Update()
     {
-        // WASD Movement inputs
-        Vector2 inputVector = new Vector2 (0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position +Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+
+        if (!canMove)
+        {
+            // cannot move towards this moveDir
+
+            // attempt only x movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            
+            if (canMove)
+            {
+                // can only move on the X axis
+                moveDir = moveDirX;
+
+            }
+            else
+            {
+                // cannot move only on X
+
+                //Attempt to move only on z
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+
+                if (canMove)
+                {
+                    // can only move on the X axis
+                    moveDir = moveDirZ;
+
+                }
+                else
+                {
+                    // cannot move in any direction.
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
 
         isWalking = moveDir != Vector3.zero;
 
