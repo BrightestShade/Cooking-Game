62460c311093b7b4b4e9182fe7c583dfe9ce637c
BrightestShade <raphaelwood9@gmail.com>
2026-07-07 20:37:17 +0100

    Container anims + code + Counter refactoring

---
+++ CookingGame/Assets/Scripts/ContainerCounter.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class ContainerCounter : BaseCounter
+{
+
+    public event EventHandler OnPlayerGrabbedObject;
+
+    [SerializeField] private KitchenObjectsSO kitchenObjectSO;
+
+   
+
+    public override void Interact(Player player)
+    {
+        if (!HasKitchenObject())
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+            OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+        }
+       
+    }
+
+
+    
+}
