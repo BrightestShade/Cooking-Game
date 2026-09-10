48ebbecb78662497850999274792d1d0204218f9
BrightestShade <raphaelwood9@gmail.com>
2026-09-10 16:44:54 +0100

    Options menu

--- CookingGame/Assets/SoundManager.cs
+++ CookingGame/Assets/SoundManager.cs
@@ -5,14 +5,19 @@ using System;
 public class SoundManager : MonoBehaviour
 {
 
+    private const string PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SoundEffectsVolume";
+
     public static SoundManager Instance { get; private set; }
 
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
 
+    private float volume = 1f;
 
     private void Awake()
     {
         Instance = this;
+
+        volume =  PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, 1f);
     }
 
     private void Start()
@@ -70,9 +75,9 @@ public class SoundManager : MonoBehaviour
 
     }
 
-    private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultiplier = 1f)
     {
-        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+        AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplier);
 
     }
 
@@ -80,5 +85,24 @@ public class SoundManager : MonoBehaviour
     {
         PlaySound(audioClipRefsSO.footstep, position, volume);
     }
+
+    public void ChangeVolume()
+    {
+        volume += 0.1f;
+        if ( volume > 1f)
+        {
+            volume = 0f;
+        }
+
+       PlayerPrefs.SetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, volume);
+       PlayerPrefs.Save();
+
+    }
+
+    public float GetVolume()
+    {
+        return volume;
+    }
+
 
 }
