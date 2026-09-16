07aa33400b1528b73ef3ff5465334ef7d9f7aeb6
BrightestShade <raphaelwood9@gmail.com>
2026-09-16 19:08:28 +0100

    Some polish

---
+++ CookingGame/Assets/Scripts/TutorialUI.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using TMPro;
+
+public class TutorialUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI keyMoveUpText;
+    [SerializeField] private TextMeshProUGUI keyMoveDownText;
+    [SerializeField] private TextMeshProUGUI keyMoveRightText;
+    [SerializeField] private TextMeshProUGUI keyMoveLeftText;
+    [SerializeField] private TextMeshProUGUI keyInteractText;
+    [SerializeField] private TextMeshProUGUI keyInteractAlternateText;
+    [SerializeField] private TextMeshProUGUI keyPauseText;
+    [SerializeField] private TextMeshProUGUI keyGamepadInteractText;
+    [SerializeField] private TextMeshProUGUI keyGamepadInteractAlternateText;
+    [SerializeField] private TextMeshProUGUI keyGamepadPauseText;
+
+}
